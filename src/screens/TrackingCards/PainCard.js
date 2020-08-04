50a32d99915dbcb883acabb59e7cd6022e537aa6
LianneChacko <liannesavio@gmail.com>
2020-08-05 00:50:51 +1200

    Exercise and Pain card changed for multiple logs

--- src/screens/TrackingCards/PainCard.js
+++ src/screens/TrackingCards/PainCard.js
@@ -12,46 +12,7 @@ import { utcToLocal, localToUtcDate, localToUtcDateTime } from "../../helpers/Da
 import { mapListItemsToTags } from "../../helpers/TagHelpers"
 const { width } = Dimensions.get('window');
 export default class PainCard extends React.Component {
-    painTags = [
-        {
-            id: 'Stomach',
-            name: 'Stomach'
-        },
-        {
-            id: 'Abdominal',
-            name: 'Abdominal'
-        },
-        {
-            id: 'Gut',
-            name: 'Gut'
-        },
-        {
-            id: 'Left Side',
-            name: 'Left Side'
-        },
-        {
-            id: 'Right Side',
-            name: 'Right Side'
-        },
-        {
-            id: 'Back',
-            name: 'Back'
-        },
-    ];
-    painTypeTags = [
-        {
-            id: 'Sharp',
-            name: 'Sharp'
-        },
-        {
-            id: 'Dull',
-            name: 'Dull'
-        },
-        {
-            id: 'Throbbing',
-            name: 'Throbbing'
-        },
-    ]
+   
     constructor(props) {
         super(props);
         this.state = { painVisible: false };
@@ -66,7 +27,7 @@ export default class PainCard extends React.Component {
             painDetails: initPainDetails(0, moment().format('YYYY-MM-DD')),
             painTypes: [],  //all possible pain types from list item
             painLocations: [], // all possible pain locations from list item
-            isPainDataAvailable: false,
+            //isPainDataAvailable: false,
             currentDate: moment().format('YYYY-MM-DD')// / this.props.route.params.CurrentDate    
         };
         this.savePainDetails = this.savePainDetails.bind(this);
@@ -116,50 +77,50 @@ export default class PainCard extends React.Component {
                 .catch((err) => console.log(err))
         );
     };
-    getUserPain = (route) => {
-        let userId = this.state.userDetails.user_id;
-        let currentDate = this.props && this.props.route && this.props.route.params && this.props.route.params.currentDate || moment().format('YYYY-MM-DD');
-        let url = constants.USERPAIN_DEV_URL.replace("[userId]", userId).replace(
-            "[occurredDate]",
-            localToUtcDateTime(currentDate)
-        );
+    // getUserPain = (route) => {
+    //     let userId = this.state.userDetails.user_id;
+    //     let currentDate = this.props && this.props.route && this.props.route.params && this.props.route.params.currentDate || moment().format('YYYY-MM-DD');
+    //     let url = constants.USERPAIN_DEV_URL.replace("[userId]", userId).replace(
+    //         "[occurredDate]",
+    //         localToUtcDateTime(currentDate)
+    //     );
        
-        getData(constants.JWTKEY).then((jwt) =>
-            fetch(url, {
-                //calling API
-                method: "GET",
-                headers: {
-                    Authorization: "Bearer " + jwt, //Passing this will authorize the user
-                },
-            })
-                .then((response) => response.json())
-                .then((responseData) => {
-                    // If responseData is not empty, then isPainDataAvailable = true
+    //     getData(constants.JWTKEY).then((jwt) =>
+    //         fetch(url, {
+    //             //calling API
+    //             method: "GET",
+    //             headers: {
+    //                 Authorization: "Bearer " + jwt, //Passing this will authorize the user
+    //             },
+    //         })
+    //             .then((response) => response.json())
+    //             .then((responseData) => {
+    //                 // If responseData is not empty, then isPainDataAvailable = true
                    
-                    if (Object.keys(responseData).length) {
+    //                 if (Object.keys(responseData).length) {
                       
-                        this.setState({
-                            isPainDataAvailable: true,
-                            painDetails: responseData,
-                            painValue: responseData.pain.pain_level,
-                            currentDate: currentDate
-                        });
-                    }
-                    else {
-                        this.setState({
-                            isPainDataAvailable: false,
-                            painDetails: initPainDetails(userId, currentDate),
-                            painValue: 0,
-                            currentDate: currentDate
-                        });
-                    }
-                })
-                .catch((err) => console.log(err))
-        );
-    };
+    //                     this.setState({
+    //                         isPainDataAvailable: true,
+    //                         painDetails: responseData,
+    //                         painValue: responseData.pain.pain_level,
+    //                         currentDate: currentDate
+    //                     });
+    //                 }
+    //                 else {
+    //                     this.setState({
+    //                         isPainDataAvailable: false,
+    //                         painDetails: initPainDetails(userId, currentDate),
+    //                         painValue: 0,
+    //                         currentDate: currentDate
+    //                     });
+    //                 }
+    //             })
+    //             .catch((err) => console.log(err))
+    //     );
+    // };
     savePainDetails() {
      
-        if (!this.state.isPainDataAvailable) {
+        // if (!this.state.isPainDataAvailable) {
             // Add the saved pain level
             let userId = this.state.userDetails.user_id;
             let occurredDate = moment(this.state.currentDate).add(moment().hour(), 'hour').add(moment().minute(), 'minute');
@@ -201,11 +162,11 @@ export default class PainCard extends React.Component {
                         return response.json();
                     })
             );
-        }
-        else {
+        // }
+        // else {
             
-            alert("Update not implemented yet.");
-        }
+        //     alert("Update not implemented yet.");
+        // }
     }
     componentDidMount() //after Ui has been uploaded 
      {
@@ -215,29 +176,29 @@ export default class PainCard extends React.Component {
             this.setState({
                 userDetails: JSON.parse(data),
             });
-            this.getUserPain();
+           // this.getUserPain(); //delete
             this.getPainTypes();
             this.getPainLocations();
         });
     }
     render() {
       
-        let painLevel = this.state.painDetails && this.state.painDetails.pain && this.state.painDetails.pain.pain_level || 0;
-
+        let painLevel =  0;
+        //this.state.painDetails && this.state.painDetails.pain && this.state.painDetails.pain.pain_level || 0
         let painLocations = this.state.painLocations || [];
-        let selectedPainLocations = [];
-        if (this.state.painDetails && this.state.painDetails.pain && this.state.painDetails.pain.locations) {
-            selectedPainLocations = mapListItemsToTags(this.state.painDetails.pain.locations);
+        // let selectedPainLocations = [];
+        // if (this.state.painDetails && this.state.painDetails.pain && this.state.painDetails.pain.locations) {
+        //     selectedPainLocations = mapListItemsToTags(this.state.painDetails.pain.locations);
             
-        }
+        // }
         let painTypes = this.state.painTypes || [] ; // get all the possible value from the list item , if not then empty array .
-        let selectedPainTypes = [];
+        //let selectedPainTypes = [];
         
-        if (this.state.painDetails && this.state.painDetails.pain && this.state.painDetails.pain.pain_type) {
-            selectedPainTypes = mapListItemsToTags([{list_item_id: this.state.painDetails.pain.pain_type,list_item_name:"Sharp"}]);
+        // if (this.state.painDetails && this.state.painDetails.pain && this.state.painDetails.pain.pain_type) {
+        //     selectedPainTypes = mapListItemsToTags([{list_item_id: this.state.painDetails.pain.pain_type,list_item_name:"Sharp"}]);
           
 
-        }
+        // }
         
         return (
             <Layout style={TrackingStyles.container}>
@@ -278,13 +239,13 @@ export default class PainCard extends React.Component {
                                 selectedTagStyle={TrackingStyles.tagStyle}
                                 maxHeight={70}
                                 tags={painLocations}
-                                selectedTags = {selectedPainLocations}
+                                // selectedTags = {selectedPainLocations}
                                 onChange={(selected) => this.setState({ selectedTags: selected })}
                             />
                         </View>
                         <Text style={{ color: '#B3B3B3', textAlign: 'left', top:hp('12%'), fontSize: wp('4%') }}>What type of pain did you experience?</Text>
                         <View style={{ top: hp('14%'), left: wp('5%')}}>
-                        <Text> Selected: {selectedPainTypes.map(tag => `${tag} `)} </Text> 
+                        {/* <Text> Selected: {selectedPainTypes.map(tag => `${tag} `)} </Text>  */}
                         
                             <TagSelector
                                 selectedTagStyle={TrackingStyles.tagStyle}
