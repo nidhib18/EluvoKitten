b544c0238dfd191c9565f14ab7802218dd031260
nidhib18 <nidhi19982@gmail.com>
2020-08-12 12:48:47 +1200

    deleted file

--- App.js
+++ App.js
@@ -3,28 +3,26 @@ import config from './aws-exports'
 
 Amplify.configure(config)
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { default as theme } from './theme.json'
 import { AppNavigator } from './src/screens/NavigationComponent';
-import ErrorBoundary from "./src/screens/ErrorBoundary"
+
 
 console.disableYellowBox = true;
 //console.reportErrorsAsExceptions = false;
 
 export default () => (
-  
+
   <>
-    <IconRegistry icons={EvaIconsPack}/>
+    <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-    <ErrorBoundary>
-      <AppNavigator/>
-    </ErrorBoundary>    
+      <AppNavigator />
     </ApplicationProvider>
-    
+
   </>
-  
+
 );
 
